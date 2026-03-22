40ab5ebf9b315f0ccd8f9bf27de6af97a7ffa738
aaronkwok0551 <aaronkwok0551@gmail.com>
2026-03-22 22:40:49 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -225,7 +225,7 @@ def process_grouped_news():
         ("📜 文匯即時", "https://politepaul.com/fd/C499xnjIBdRm.xml"),
         ("🔵 點新聞即時", "https://politepaul.com/fd/xbfGvXWovqfk.xml"),
         ("🔵 點新聞評論", "https://politepaul.com/fd/59PndwU1mb82.xml"),
-        ("🔵 文匯評論", "https://politepaul.com/fd/6oljXv2E75Pp.xml")
+        ("🔵 文匯評論", "https://politepaul.com/fd/6oljXv2E75Pp.xml"),
         ("🔵 商台即時", "https://politepaul.com/fd/4xPuKWS07tJs.xml")
     ]
     
